9238d3979dab9e35c85e9a5c687cb288955b15c9
William <wbeard8@gatech.edu>
2020-09-21 02:09:13 +0000

    Changed 'Wheel' to 'wheel' in exercise 1.1

--- exercise_1_1/exercise_1_1.cpp
+++ exercise_1_1/exercise_1_1.cpp
@@ -49,7 +49,7 @@ int main() {
         }
         //
         for (int i = 0; i < 4; i++) {
-        	std::cout << "Wheel at index " << i << " has speed: " << wheel_speeds[i] << std::endl;
+        	std::cout << "wheel at index " << i << " has speed: " << wheel_speeds[i] << std::endl;
         }
     }
     return 0;
